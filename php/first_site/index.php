6b2ed55fdd321d8fda846eed934cfe3fdb870dc8
Tobib-11 <tobias.beran@student.htldornbirn.at>
2026-05-26 15:22:31 +0200

    26.05.2026 mit css verknüpfen

---
+++ php/first_site/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Test</title>
+  <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+
+<h1>hallo</h1>
+
+
+<?php
+    $hello = 'hello my friend';
+    
+    
+?> 
+<p class="hello"><?php echo $hello; ?></p>
+
+</body>
+</html>
